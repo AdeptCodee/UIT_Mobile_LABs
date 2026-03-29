3e21005e9eb28b4e592c6866082a7ea006571d80
AdeptR <truongmanhnguyena@gmail.com>
2026-03-29 14:47:48 +0700

    add CV button to Main screen

--- LAB2/app/src/main/java/com/example/ex_flappy_x/MainActivity.java
+++ LAB2/app/src/main/java/com/example/ex_flappy_x/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.ex_flappy_x;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-
+import androidx.cardview.widget.CardView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     ImageView btn_play;
+    CardView btn_credits;
+    View profileLayout;
+    TextView txt_credits_label;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +28,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btn_credits = findViewById(R.id.btn_credits);
+        profileLayout = findViewById(R.id.include_profile);
+        txt_credits_label = findViewById(R.id.txt_credits_label);
         btn_play = findViewById(R.id.btn_play);
+
         btn_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Play!!", Toast.LENGTH_SHORT).show();
             }
+        });
+
+        btn_credits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (profileLayout.getVisibility() == View.GONE) {
+                    profileLayout.setVisibility(View.VISIBLE);
+                    txt_credits_label.setText("Back"); // Đổi chữ nút thành Back
+
+                    // Ẩn nút Play và các thành phần khác , màn hình sạch hơn
+                    btn_play.setVisibility(View.GONE);
+                }
+                // nếu Profile đang hiện thì ẩn nó đi
+                else {
+                    profileLayout.setVisibility(View.GONE);
+                    txt_credits_label.setText("Ex1"); // Đổi chữ nút quay lại ban đầu
+
+                    // Hiện lại nút Play
+                    btn_play.setVisibility(View.VISIBLE);
+                }
+            }
+
         });
     }
 }
