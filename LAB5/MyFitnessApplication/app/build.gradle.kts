ef05952f1f5fdeea3a86c023ffbd808a6af2db8d
AdeptR <truongmanhnguyena@gmail.com>
2026-06-06 23:30:13 +0700

    hi

---
+++ LAB5/MyFitnessApplication/app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.example.myfitnessapplication"
+
+    // SỬA CHỖ NÀY: Dùng số nguyên trực tiếp
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.myfitnessapplication"
+        minSdk = 24
+
+        // SỬA CHỖ NÀY: Dùng số nguyên 34
+        targetSdk = 34
+
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Room Database
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    add("ksp", "androidx.room:room-compiler:$room_version")
+}
